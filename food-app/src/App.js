2d1c082c831fb4aa5120cc971fcdce42c414deb5
Abhijith Acharya <abhijith.acharya@epsilon.com>
2024-07-17 20:48:19 +0530

    signup form styling

--- food-app/src/App.js
+++ food-app/src/App.js
@@ -2,12 +2,13 @@ import './styles/index.scss';
 import { Outlet } from 'react-router-dom';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
-
+import Signup from './Models/Signup';
 function App() {
   return (<div className='app'>
     <Header />
     <Outlet />
     <Footer />
+    <Signup/>
   </div>);
 }
 
