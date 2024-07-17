39ac70a347744d992d0ddffdc8eeae0b6c02194b
witcher077 <ashokkumarmahto2000@gmail.com>
2024-07-18 00:17:30 +0530

    submit handle

--- food-app/src/Models/Signup.jsx
+++ food-app/src/Models/Signup.jsx
@@ -1,29 +1,52 @@
 import React from 'react'
+import { useState } from 'react'
 
 function Signup() {
-  return (
-        <form className='signup-form'>
-            <div className="name-label">
+    const [userData, setUserData] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: ''
+    });
+
+    const HandleChange = (e) => {
+        const { name, value } = e.target;
+        setUserData({
+            ...userData,
+            [name]: value
+        })
+    }
+
+    const HandleSubmit = (e) => {
+        e.preventDefault();
+        console.log(userData);
+        localStorage.setItem('email', userData['email']);
+        localStorage.setItem('password', userData['password']);
+    }
+
+    return (
+        <form className='signup-form' onSubmit={HandleSubmit} >
+ <div className="name-label">
                 <label htmlFor="first-name">Name</label>
-                <input type="text" id='first-name' placeholder='John'/>
+                <input onChange={HandleChange} value={userData.firstName} name='firstName' type="text" id='first-name' placeholder='John' />
             </div>
-            <div className="lastname-label">
+          <div className="lastname-label">
                 <label htmlFor="last-name">Last Name</label>
-                <input type="text" id='last-name' placeholder='Doe'/>
+                <input onChange={HandleChange} value={userData.lastName} name='lastName' type="text" id='last-name' placeholder='Doe' />
             </div>
             <div className="email-label">
                 <label htmlFor="email">Email</label>
-                <input type="email" id='email' placeholder='example@domain.com'/>
+                <input onChange={HandleChange} value={userData.email} name='email' type="email" id='email' placeholder='example@domain.com' />
             </div>
             <div className="password-label">
                 <label htmlFor="password">Password</label>
-                <input type="password" id='password' placeholder='********'/>
+                <input onChange={HandleChange} value={userData.password} name='password' type="password" id='password' placeholder='********' />
             </div>
             <div className="submit">
                 <button className='submit-btn' type='submit'>Create Account</button>
             </div>
         </form>
-  )
+    )
 }
 
 export default Signup
